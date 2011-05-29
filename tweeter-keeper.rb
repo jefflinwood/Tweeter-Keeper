50653fb64f08e0f7bcd7de70f61affdd8283683f
Jeff Linwood <jlinwood@gmail.com>
2011-05-29 11:00:21 -0500

    Added config file and ability to follow Twitter users

--- tweeter-keeper.rb
+++ tweeter-keeper.rb
@@ -1,6 +1,10 @@
 require 'rubygems'
 require 'tweetstream'
 require 'mongo'
+require 'twitter'
+require 'yaml'
+
+@config = YAML.load_file('config.yaml')
 
 #set up a connection to a local MongoDB
 @db = Mongo::Connection.new.db("tweeterkeeper")
@@ -9,15 +13,20 @@ require 'mongo'
 @tweets = @db.collection("tweets")
 
 @tracking_keywords = Array['drupal','mongodb','three20'];
+@follow_users = Twitter.friend_ids("jefflinwood").ids;
 
-@client = TweetStream::Client.new('USERNAME','PASSWORD')
+@client = TweetStream::Client.new(@config['username'],@config['password'])
 
 @client.on_delete do |status_id, user_id|
   puts "Removing #{status_id} from storage"
   @tweets.remove({"status" => status_id})
 end
 
-@client.track(*@tracking_keywords) do |status|
+@params = Hash.new;
+@params[:follow] = @follow_users;
+@params[:track] = @tracking_keywords;
+
+@client.filter(@params) do |status|
   puts "#{status.text}"
  @tweets.insert(status)
 end
