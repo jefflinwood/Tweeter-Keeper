0a9f82d6260a56a763a9fd0a454d6184473cc8fc
Jeff Linwood <jlinwood@gmail.com>
2011-08-11 15:27:54 -0500

    Added MongoLab support for Heroku

--- tweeter-keeper.rb
+++ tweeter-keeper.rb
@@ -3,30 +3,40 @@ require 'tweetstream'
 require 'mongo'
 require 'twitter'
 require 'yaml'
+require 'date'
+require 'time'
 
-@config = YAML.load_file('config.yaml')
 
-#set up a connection to a local MongoDB
-@db = Mongo::Connection.new.db("tweeterkeeper")
+config = YAML.load_file('config.yaml')
 
-#all tweets will be stored in a collection called tweets
-@tweets = @db.collection("tweets")
+#set up a connection to a local MongoDB or MongoLab from Heroku
+  if ENV['MONGOLAB_URI']
+    uri = URI.parse(ENV['MONGOLAB_URI'])
+    conn = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
+    db = conn.db(uri.path.gsub(/^\//, ''))
+  else
+    db = Mongo::Connection.new.db("tweeterkeeper")
 
-@tracking_keywords = Array['drupal','mongodb','three20'];
-@follow_users = Twitter.friend_ids("jefflinwood").ids;
+  end
 
-@client = TweetStream::Client.new(@config['username'],@config['password'])
+#all tweets will be stored in a collection
+tweets = db.collection("app-tweets")
 
-@client.on_delete do |status_id, user_id|
+tracking_keywords = Array['aplusk'];
+follow_users = Twitter.friend_ids("jefflinwood").ids;
+
+client = TweetStream::Client.new(config['username'],config['password'])
+
+client.on_delete do |status_id, user_id|
   puts "Removing #{status_id} from storage"
-  @tweets.remove({"status" => status_id})
+  tweets.remove({"status" => status_id})
 end
 
-@params = Hash.new;
-@params[:follow] = @follow_users;
-@params[:track] = @tracking_keywords;
+params = Hash.new;
+params[:follow] = follow_users;
+params[:track] = tracking_keywords;
 
-@client.filter(@params) do |status|
-  puts "#{status.text}"
- @tweets.insert(status)
+client.filter(params) do |status|
+  puts "#{status.text} - #{status.created_at}"
+  tweets.insert(status)
 end
